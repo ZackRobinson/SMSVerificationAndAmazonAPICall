85474d31e659a34317bbc6a072d36912e4a8fe64
Zack Robinson <zwrprofessional@gmail.com>
2017-09-25 09:41:28 -0400

    Fixed the messageReceived method in main

--- app/src/main/java/com/zackeryrobinson/smsverificationandamazonapicall/MainActivity.java
+++ app/src/main/java/com/zackeryrobinson/smsverificationandamazonapicall/MainActivity.java
@@ -1,26 +1,61 @@
 package com.zackeryrobinson.smsverificationandamazonapicall;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityTag";
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    TextView tvSMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSMS = (TextView) findViewById(R.id.tvSMS);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 123);
+        } else {
+        }
+
+//        try {
+//            SmsManager smsManager = SmsManager.getDefault();
+//            smsManager.sendTextMessage("8145251519", null, "Meow", null, null);
+//            Toast.makeText(MainActivity.this, "Message sent", Toast.LENGTH_LONG).show();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
         SmsReceiver.bindListener(new SmsListener() {
             @Override
             public void messageReceived(String messageText) {
                 Log.d(TAG, "messageReceived: ");
-                Toast.makeText(MainActivity.this, "Message: "+messageText,Toast.LENGTH_LONG).show();
+                tvSMS.setText(messageText);
+                Toast.makeText(MainActivity.this, "Message: " + messageText, Toast.LENGTH_LONG).show();
             }
         });
 
+
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+//            if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+//                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+//                String[] permissions = {Manifest.permission.SEND_SMS};
+//                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+//            }
+//        }
 
 
     }
--- app/src/main/java/com/zackeryrobinson/smsverificationandamazonapicall/SmsReceiver.java
+++ app/src/main/java/com/zackeryrobinson/smsverificationandamazonapicall/SmsReceiver.java
@@ -5,23 +5,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
 
+    private static final String TAG = "SMSReceiverTag";
     private static SmsListener mListener;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: ");
         Bundle data = intent.getExtras();
 
         Object[] pdus = (Object[]) data.get("pdus");
 
         for (int i = 0; i < pdus.length; i++) {
-            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
-            
+            SmsMessage smsMessage = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            }
+
             String sender = smsMessage.getMessageBody();
             //You must check if the sender is your provider and not another one with same text.
-
+            Log.d(TAG, "onReceive: Got MessageBody");
             String messageBody = smsMessage.getMessageBody();
 
             //Pass on the text to our listener.
